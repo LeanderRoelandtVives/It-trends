e09dd102896b440e8da8032dabc992a9510ebe20
LeanderRoelandtVives <leander.roelandt@student.vives.be>
2026-03-16 11:26:22 +0100

    Changed base route and login logout and register returns to menu page

--- Application/RestaurantAi/RestaurantAi.Mvc/Program.cs
+++ Application/RestaurantAi/RestaurantAi.Mvc/Program.cs
@@ -53,7 +53,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Menu}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 app.Run();
